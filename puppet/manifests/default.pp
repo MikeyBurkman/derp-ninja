abc817712c78f71678ed9be659d551af36aca001
Michael Burkman <michael.burkman@gmail.com>
2014-09-18 21:17:21 -0400

    Added vagrant and puppet config

---
+++ puppet/manifests/default.pp
@@ -0,0 +1,58 @@
+
+$as_vagrant   = 'sudo -u vagrant -H bash -l -c'
+$home         = '/home/vagrant'
+
+Exec {
+  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin']
+}
+
+# --- Preinstall Stage ---------------------------------------------------------
+
+stage { 'preinstall':
+  before => Stage['main']
+}
+
+class apt_get_update {
+  exec { 'apt-get -y update':
+    unless => "test -e ${home}/.rvm"
+  }
+}
+class { 'apt_get_update':
+  stage => preinstall
+}
+
+# --- Memcached ----------------------------------------------------------------
+
+#class { 'memcached': }
+
+# --- Packages -----------------------------------------------------------------
+
+package { 'curl':
+  ensure => installed
+}
+
+package { 'build-essential':
+  ensure => installed
+}
+
+package { 'git-core':
+  ensure => installed
+}
+
+# Nokogiri dependencies.
+package { ['libxml2', 'libxml2-dev', 'libxslt1-dev']:
+  ensure => installed
+}
+
+# ExecJS runtime.
+package { 'nodejs':
+  ensure => latest
+}
+
+package { 'mongodb': 
+  ensure => latest
+}
+
+package { 'npm':
+  ensure => latest
+}
