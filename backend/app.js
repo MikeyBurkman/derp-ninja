e52133b6f868a211f208c923653fa1ee501bdfc6
Michael Burkman <michael.burkman@gmail.com>
2014-12-21 12:06:54 -0500

    fixed app.js

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,8 @@
 
-var eggnog = require('eggnog');
+var eggnog = require('../../eggnog');
 
 var context = eggnog.newContext();
-context.scanForFiles({
-	baseDir: __dirname, 
-	excludeFn: function(fname) {
-	console.log('fname: ', fname);
-	return false;
-}});
+context.scanForFiles(__dirname);
 
 context.main();
 
