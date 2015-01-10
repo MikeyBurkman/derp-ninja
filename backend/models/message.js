83ecd491768f8fac99e9c8089b705312edf1374f
Michael Burkman <michael.burkman@gmail.com>
2015-01-10 09:14:11 -0500

    typo

--- backend/models/message.js
+++ backend/models/message.js
@@ -11,11 +11,11 @@ function init(imports) {
 
 	var mongoose = require('mongoose');
 	var Schema = mongoose.Schema;
-	
+
 	var MessageSchema = new Schema({
 	    user: {type: String, ref: 'User'},
 	    messageText: String,
-    	tags: [type:String],
+    	tags: {type:String},
 	    created: {type: Date, default: new Date()}
 	});
 
